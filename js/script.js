d5a87b90dcf380cf6a3023ec57e216d064b8e271
alexandre <lemoine.alexandre.b@gmail.com>
2020-10-26 13:56:36 +0100

    renew table note + display all notes eleve

--- js/script.js
+++ js/script.js
@@ -6,8 +6,42 @@ listeEleve.push(new Eleve("Jean","Dupont","27"));
 listeEleve.push(new Eleve("Napoléon","Bonaparte","18"));
 listeEleve.push(new Eleve("Winston","Churchill","44")); 
 
+
 // Injection des élèves dans la liste déroulante
 var displayListeEleve = document.getElementById("listeEleve");
+
+
+function displayNotesEleve(){
+    
+    // affichage des informations de l'éleve
+    document.getElementById('nomEleve').innerHTML = listeEleve[displayListeEleve.value].nom;
+    document.getElementById('prenomEleve').innerHTML = listeEleve[displayListeEleve.value].prenom;
+    document.getElementById('ageEleve').innerHTML = listeEleve[displayListeEleve.value].age;
+    
+    //suppresion des lignes
+    var tbody = document.getElementById('contentNotes');
+    tbody.innerHTML = '';
+
+    listeEleve[displayListeEleve.value].matieres.forEach(function(matiere){
+
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+    
+        td.innerHTML = matiere.nom;
+        tr.appendChild(td); 
+        
+        matiere.notes.forEach(function(note){
+            let td = document.createElement('td');
+            td.innerHTML = note;
+            tr.appendChild(td);
+        });
+        
+        tbody.appendChild(tr);
+    
+    });
+
+
+}
 
 listeEleve.forEach(function(eleve,index){
 
@@ -18,10 +52,3 @@ listeEleve.forEach(function(eleve,index){
     
 });
 
-function displayNotesEleve(){
-  
-    document.getElementById('nomEleve').innerHTML = listeEleve[displayListeEleve.value].nom;
-    document.getElementById('prenomEleve').innerHTML = listeEleve[displayListeEleve.value].prenom;
-    document.getElementById('ageEleve').innerHTML = listeEleve[displayListeEleve.value].age;
-    
-}
